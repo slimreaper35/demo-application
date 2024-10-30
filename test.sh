594c9fd699b6f9262571f946223a0e59e6ce4b46
Michal Šoltis <msoltis@redhat.com>
2024-10-30 09:13:31 +0100

    fix Dockerfile
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source ~/cachi2/venv/bin/activate
+
 cachi2 fetch-deps bundler
 cachi2 inject-files --for-output-dir /tmp/cachi2-output cachi2-output
 cachi2 generate-env --for-output-dir /tmp/cachi2-output --output cachi2.env cachi2-output
